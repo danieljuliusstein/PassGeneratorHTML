9598045d4927d018369f591aaca43555dd90356c
Daniel Julius Stein <69329733+danieljuliusstein@users.noreply.github.com>
2023-08-16 23:08:56 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+const copy = document.querySelector("#copy-button");
+const generate = document.querySelector(".generate-button");
+const button = document.querySelector('#button'); 
+let uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let lowercase = "abcdefghijklmnopqrstuvwxyz";
+let symbols = "!@#$%^&*()_+[]{}?><";
+let numbers = "1234567890";
+
+function copy_func() {
+  event.preventDefault();
+  var copyText = document.getElementById("pass");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999);
+  navigator.clipboard.writeText(copyText.value);
+  alert("Copied the text");
+}
+
+function generate_func() {
+  event.preventDefault();
+  let password = "";
+  var text = document.getElementById('pass');
+  var numInput = document.getElementById("number").value;
+  
+  for (let i = 0; i < numInput; i++) {
+    if (document.getElementById('accept').checked) {
+      password += lowercase[Math.floor(Math.random() * lowercase.length)];
+    }
+  }
+  for (let i = 0; i < numInput; i++) {
+    if (document.getElementById('accept1').checked) {
+      password += uppercase[Math.floor(Math.random() * uppercase.length)];
+    }
+  }  
+  for (let i = 0; i < numInput; i++) {
+    if (document.getElementById('accept2').checked) {
+      password += symbols[Math.floor(Math.random() * symbols.length)];
+    }
+  }  
+  for (let i = 0; i < numInput; i++) {
+    if (document.getElementById('accept3').checked) {
+      password += numbers[Math.floor(Math.random() * numbers.length)];
+    }
+  }  
+  let newPassword = ""; 
+  
+  for (let i = 0; i < numInput; i++) {
+    newPassword += password[Math.floor(Math.random() * password.length)];  
+  }
+  text.value = newPassword; 
+  console.log(text.value);
+}
